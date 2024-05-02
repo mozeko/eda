682d7aa6101a293e6e58461c6d13a574851a0ccd
Zakaria Mostafa <149090419+mozeko@users.noreply.github.com>
2024-05-02 23:14:11 +0400

    Add files via upload

---
+++ 1_🤓Home.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import streamlit as st
+
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="👋",
+)
+
+st.title("  Flight Fare Price Prediction App  ✈")
+st.sidebar.success("Select a page above.")
+st.sidebar.image("fl.jpg")
+st.sidebar.markdown("Mad With :heart_eyes: by Analyst.[Zakaria Mostafa](https://www.linkedin.com/in/zakariamostafa/) ")
+st.write('''Airlines play a pivotal role in global transportation, connecting
+            people and goods across vast distances. They operate a complex network of
+            routes, managing everything from aircraft maintenance to customer service
+            Airlines own or lease a fleet of aircraft to transport passengers and cargo.
+            These fleets can range from small regional jets to large intercontinental planes.
+            Airlines factor in various operating costs, including fuel, aircraft maintenance, crew salaries,
+            airport fees, and insurance. Ticket prices fluctuate based on supply and demand dynamics. Popular 
+            routes during peak travel seasons often have higher prices. Prices are influenced by competition within
+            the airline industry. Airlines may adjust their fares to remain competitive with other carriers operating
+            on the same routes Airlines generate additional revenue through ancillary services like baggage fees, seat 
+            selection, in-flight meals, and entertainment In essence, airline ticket prices are the result of a complex 
+          interplay between operational costs, market forces, and consumer behavior. By understanding these factors,
+          travelers can make informed decisions when booking flights.''')
